cab1cd2456b32e87c61b5db387fde6833def0404
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-01-05 15:34:53 +0000

    added and is not zero to greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ num_str = repr(number)
 last_digit_str = num_str[-1]
 last_digit = int(last_digit_str)
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5 and not zero")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6  and last_digit != 0:
