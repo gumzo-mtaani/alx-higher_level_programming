35c3faa54d56f85332253f0e420871389a462465
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-02-11 09:52:05 +0300

    corrections added to max integer task

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) == 0:
-        return None
-    max = my_list[0]
-    for i in my_list:
-        if max < i:
-            max = i
-    return 
+    if len(my_list):
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
+    return None
