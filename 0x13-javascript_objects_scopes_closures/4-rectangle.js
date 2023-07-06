88c857a5f5999b607ac26611b1eb5b38de8b5fbc
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-07-06 18:25:02 +0300

    corrections

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,33 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let h = this.height; h > 0; h--) {
-      console.log('X'.repeat(this.width));
+    for (let i = 1; i <= this.height; i++) {
+      let row = '';
+      for (let j = 1; j <= this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
   }
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
+
   double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    this.height *= 2;
+    this.width *= 2;
   }
-};
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+}
+
+module.exports = Rectangle;
+
