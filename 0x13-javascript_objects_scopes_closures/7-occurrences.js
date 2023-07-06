aa1cab14d3360804ec4f284dd4f32f5e37bb8aa2
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-07-06 18:22:51 +0300

    corrections

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
-module.exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (let ele in list) {
-    if (list[ele] === searchElement) {
-      count++;
-    }
-  }
-  return count;
+exports.nbOccurences = function (list, searchElement) {
+  const occurences = list.filter(item => item === searchElement).length;
+  return occurences;
 };
